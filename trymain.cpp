20f12799ef9debe63f3be860449c6935c95c589d
anamarigarzon <63021584+anamarigarzon@users.noreply.github.com>
2020-11-10 18:24:30 -0500

    Update trymain.cpp

--- trymain.cpp
+++ trymain.cpp
@@ -5,8 +5,43 @@ using namespace std;
 
 int main(){
 
-  Linea4 juego = Linea4();
-  juego.play();
+  int b;
+  int h;
 
+  cout << "Bienvenido a Cuatro en Linea" << endl;
+
+  bool playing = true;
+
+  while(playing){
+    Linea4 juego;
+    cout << "Individual [0] o Multijugador [1]? ";
+    cin >> b;
+
+    if(b == 0){
+      juego.playAI();
+      cout << endl << "Jugar de nuevo?" << endl;
+      cout << "[1] si" << endl;
+      cout << "[0] no" << endl;
+      cin >> h;
+        if(h != 1){
+          playing == false;
+          break;
+        }
+
+    }else if(b == 1){
+      juego.play2players();
+      cout << endl << "Jugar de nuevo?" << endl;
+      cout << "[1] si" << endl;
+      cout << "[0] no" << endl;
+      cin >> h;
+        if(h != 1){
+          playing == false;
+          break;
+        }
+    }
+  }
+
+  cout << endl << "Gracias por jugar 4 en linea" << endl;
+  cout << endl << "Hasta la proxima" << endl;
   return 0;
 }
